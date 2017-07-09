ec7314c987469fdfe65a704ad56b12a9fca72269
Ali AYOUBI <ali.m.ayoubi@gmail.com>
2017-07-09 13:21:47 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='spyklines',
+      version='1.0',
+      description='Create simple and neat sparklines with Matplotlib',
+      url='',
+      author='aayoubi',
+      author_email='',
+      license='MIT',
+      packages=['spyklines'],
+      zip_safe=False)
