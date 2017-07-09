6e1c9cc9387def72d96107d6ad880d4747ef5283
Ali AYOUBI <ali.m.ayoubi@gmail.com>
2017-07-09 12:44:54 +0200

    Add a 10-line sample to create sparklines with its trendline

---
+++ bin/sample_sparkline.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from spyklines import SparkLine, TrendLine
+
+
+if __name__ == '__main__':
+    number_of_plots = 10
+    plt.figure(figsize=(6, 4))
+    for i in range(1, number_of_plots + 1):
+        ax = plt.subplot(number_of_plots, 1, i)
+        values = [np.random.randint(100, 500) for j in range(1, 100)]
+        sparkline = SparkLine('line-{}'.format(i), values)
+        sparkline.plot(ax)
+        trendline = TrendLine('line-{}'.format(i), values)
+        trendline.plot(ax)
+    plt.show()
